4941fbd144ee6a6d46ba3f95682f6474897cc34f
Lalit singhal <lalitsinghal2k6@gmail.com>
2026-05-03 15:38:06 +0530

    fix CORS issues for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,18 @@ app.use("/uploads", express.static(path.resolve("uploads")));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-app.use(cors({
-    origin: [https://ai-job-portal-backend-xdec.onrender.com]
-    credentials: true
-}));
+app.use(cors(
+    {
+        origin: ["http://localhost:5173",
+                "https://ai-job-portal-one.vercel.app"],
+        credentials:true
+    }
+//     {
+//     origin: [https://ai-job-portal-backend-xdec.onrender.com]
+//     credentials: true
+// }
+));
+
 
 const PORT = process.env.PORT || 8000;
 
